42516a30cd9e7a700635520a1846b42fda1345e8
shusueke_sato <09ks-ss-satoshu@keio.jp>
2020-12-02 21:18:23 +0900

    rotationするscriptの追加とprefabsで迷路を作るひな形の作成

---
+++ interaction/Assets/scripts/rotation_wall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation_wall : MonoBehaviour
+{
+    private Gyroscope m_gyro;
+
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        m_gyro = Input.gyro;
+        transform.rotation = m_gyro.attitude;
+    }
+}
