c653d743e359cdee414816c7483ae9447ac5e5f9
shusueke_sato <09ks-ss-satoshu@keio.jp>
2020-12-06 18:05:01 +0900

    gameover画面の追加"

--- interaction/Assets/scripts/GameSystem.cs
+++ interaction/Assets/scripts/GameSystem.cs
@@ -21,9 +21,4 @@ public class GameSystem : MonoBehaviour
     {
         SceneManager.LoadScene("dungeon_ver1");
     }
-    public void EndGame()
-    {
-        SceneManager.LoadScene("Title");
-    }
-
 }
