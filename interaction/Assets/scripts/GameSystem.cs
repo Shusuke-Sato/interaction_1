8dec8f629c7ead5dffe80fef13cb66b185541d17
DESKTOP-GAI7I5J\sandaru <k66teraoka@keio.jp>
2020-12-21 15:38:14 +0900

    missing scriptの修正

--- interaction/Assets/scripts/GameSystem.cs
+++ interaction/Assets/scripts/GameSystem.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameSystem : MonoBehaviour
 {
-    public static int stageNo = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,68 +17,13 @@ public class GameSystem : MonoBehaviour
         
     }
 
-    public void StartStage1()
+    public void StartGame()
     {
-        stageNo = 1;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void StartStage2()
-    {
-        stageNo = 2;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void StartStage3()
-    {
-        stageNo = 3;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void StartStage4()
-    {
-        stageNo = 4;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void StartStage5()
-    {
-        stageNo = 5;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void Restart()
-    {
-        SceneManager.LoadScene("Tutorial");
+        SceneManager.LoadScene("dungeon_ver1");
     }
     public void EndGame()
     {
         SceneManager.LoadScene("Title");
     }
-    public void NextGame()
-    {
-        stageNo = stageNo + 1;
-        SceneManager.LoadScene("Tutorial");
-    }
-    public void DoneTutorial()
-    {
-        Debug.Log(stageNo);
-        switch (stageNo)
-        {
-            case 1:
-                SceneManager.LoadScene("Stage1");
-                break;
-            case 2:
-                SceneManager.LoadScene("Stage2");
-                break;
-            case 3:
-                SceneManager.LoadScene("Stage3");
-                break;
-            case 4:
-                SceneManager.LoadScene("Stage4");
-                break;
-            case 5:
-                SceneManager.LoadScene("Stage5");
-                break;
-            default:
-                SceneManager.LoadScene("Title");
-                break;
-        }
-    }  
 
 }
